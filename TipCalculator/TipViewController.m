8c26bae3f58b2ea02fcc57ebe69fdc8ccdb96707
Imtiyaz Jariwala <imtiyaz@yahoo-inc.com>
2017-01-17 15:11:15 -0800

    Updates

--- TipCalculator/TipViewController.m
+++ TipCalculator/TipViewController.m
@@ -13,30 +13,42 @@
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UITextField *otherTipPerc;
+@property (weak, nonatomic) IBOutlet UILabel *otherTipPercLabel;
 
 @end
 
 @implementation TipViewController
 
-NSUserDefaults *defaultSettings;
+NSArray *tipValues;
+NSUserDefaults *defaultValues;
 NSInteger defaultTip;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.title = @"Tip Calculator";
+    [self.otherTipPerc setHidden:YES];
+    [self.otherTipPercLabel setHidden:YES];
+    
+    
+    tipValues = @[@0.15, @0.2, @0.25];
     [self updateValues];
     
-    defaultSettings = [NSUserDefaults standardUserDefaults];
-    defaultTip = [defaultSettings integerForKey:@"default_tip_percentage"];
+    defaultValues = [NSUserDefaults standardUserDefaults];
+    defaultTip = [defaultValues integerForKey:@"default_tip_percentage"];
     [self.tipControl setSelectedSegmentIndex:defaultTip];
     
+    
+    
+    [self.billTxtField becomeFirstResponder];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     NSLog(@"view will appear");
     
-    defaultTip = [defaultSettings integerForKey:@"default_tip_percentage"];
+    defaultTip = [defaultValues integerForKey:@"default_tip_percentage"];
     [self.tipControl setSelectedSegmentIndex:defaultTip];
     
     [self updateValues];
@@ -71,17 +83,46 @@ NSInteger defaultTip;
     [self updateValues];
 }
 
-- (void)updateValues {
-    float billAmount = [self.billTxtField.text floatValue];
-    
-    NSArray *tipValues = @[@0.15, @0.2, @0.25];
-    
-    float tipAmount = [tipValues[self.tipControl.selectedSegmentIndex] floatValue] * billAmount;
 
+
+- (void)updateValues {
+    float tipAmount;
+    float billAmount = [self.billTxtField.text floatValue];
+    int tipPerc;
+
+    if (self.tipControl.selectedSegmentIndex == 3) {
+        [self.otherTipPerc setHidden:NO];
+        [self.otherTipPercLabel setHidden:NO];
+        
+        tipPerc = [self.otherTipPerc.text intValue];
+        NSLog(@"TIP PERC: %@, %i", self.otherTipPerc.text, [self.otherTipPerc.text intValue]);
+        tipAmount = (tipPerc / 100.00f) * billAmount;
+    }
+    else {
+        [self.otherTipPerc setHidden:YES];
+        [self.otherTipPercLabel setHidden:YES];
+        
+        tipPerc = [tipValues[self.tipControl.selectedSegmentIndex] floatValue] * 100;
+        tipAmount = [tipValues[self.tipControl.selectedSegmentIndex] floatValue] * billAmount;
+    }
+    
+    [self updateTipAmountLabel:tipPerc];
+    
     float totalAmount = billAmount + tipAmount;
+    NSLog(@"TIP AMT: %f", tipAmount);
+    NSLog(@"TOTAL AMT: %f", totalAmount);
     
     self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
     self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+    
+}
+
+- (IBAction)onTipPercUpdate:(UITextField *)sender {
+    [self updateValues];
+}
+
+- (void) updateTipAmountLabel:(int) tipPerc {
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"Tip Amount @ %i%%", tipPerc];
 }
 
 @end
